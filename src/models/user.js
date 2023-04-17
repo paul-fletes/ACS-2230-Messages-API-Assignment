131c920851273051f074f0758b8a232319daa989
Paul Fletes <paul.fletes@gmail.com>
2023-04-17 17:10:05 -0500

    update user model and routing

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,12 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-// Add your models here.
+const UserSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, select: false },
+  messages: [{ type: Schema.Types.ObjectId, ref: 'Message' }]
+})
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = User
